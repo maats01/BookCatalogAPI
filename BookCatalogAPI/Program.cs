779962ffadfef51e690669cddeac0e9eddbdd392
maats01 <matheusp.sampaio@gmail.com>
2025-05-02 17:57:26 -0300

    change singleton to scoped in BooksService

--- BookCatalogAPI/Program.cs
+++ BookCatalogAPI/Program.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<BooksService>();
+builder.Services.AddScoped<BooksService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => 
